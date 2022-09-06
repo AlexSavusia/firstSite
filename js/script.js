6512f8b8323f73890d0de7e58ec135defa4137db
AlexSavusia <mr.savusya@gmail.com>
2022-09-06 18:34:17 +0600

    first app

--- js/script.js
+++ js/script.js
@@ -1,8 +1,19 @@
-"use strict";
+const numberOfFilms = prompt('Сколько фильмов просмотрели?', '')
 
-let incr = 10,
-    decr = 10;
+const personalMovieDB = {
+    count:numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+}
 
+const a = prompt('Один из последних просмотреннеых фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотреннеых фильмов?', ''),
+      d = prompt('На сколько оцените его?', '')
 
-console.log(++incr)
-console.log(--decr)
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB)
